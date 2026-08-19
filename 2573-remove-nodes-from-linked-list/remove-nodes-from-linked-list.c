c9dab27edff7570b4c23cb84550c5ff6ffab1e42
aryanadepu373 <aryuuscode@gmail.com>
2026-08-19 19:00:15 +0530

    Time: 28 ms (48.65%) | Memory: 70.6 MB (12.97%) - LeetSync

---
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.c
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* newnode(int val){
+    struct ListNode* temp;
+    temp=malloc(sizeof(struct ListNode));
+    temp->val=val;
+    temp->next=NULL;
+    return temp;
+}
+struct ListNode* reverse(struct ListNode* head){
+    struct ListNode *prev,*ptr,*temp;
+    prev=NULL;
+    temp=head;
+    while(temp!=NULL){
+        ptr=temp->next;
+        temp->next=prev;
+        prev=temp;
+        temp=ptr;
+    }
+    return prev;
+}
+struct ListNode* removeNodes(struct ListNode* head) {
+    int stack[100001],top=-1;
+    struct ListNode *temp;
+    temp=reverse(head);
+    while(temp!=NULL){
+        if(top!=-1){
+            if(stack[top]<=temp->val){
+                top++;
+                stack[top]=temp->val;
+            }
+        }
+        else{
+            top++;
+            stack[top]=temp->val;
+        }
+        temp=temp->next;
+    }
+    struct ListNode *temp1,*ptr,*head1;
+    temp1=newnode(0);
+    head1=temp1;
+    while(top!=-1){
+        ptr=newnode(stack[top]);
+        temp1->next=ptr;
+        temp1=ptr;
+        top--;
+    }
+    return head1->next;
+}
