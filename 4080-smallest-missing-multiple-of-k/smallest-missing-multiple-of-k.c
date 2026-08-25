2124d855189e6481b3b9470b93bd1236fd286548
aryanadepu373 <aryuuscode@gmail.com>
2026-08-25 21:01:05 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (7.89%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.c
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.c
@@ -1,9 +1,9 @@
 int missingMultiple(int* nums, int numsSize, int k) {
-    int hash[10001]={0},i;
+    int hash[1001]={0},i;
     for(i=0;i<numsSize;i++){
         hash[nums[i]]=1;
     }
-    for(i=k;i<10001;i=i+k){
+    for(i=k;i<1001;i=i+k){
         if(hash[i]!=1){
             return i;
         }
