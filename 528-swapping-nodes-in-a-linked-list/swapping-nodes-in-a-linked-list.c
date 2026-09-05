f61777f654a832b399d4df3502adeb75028d7f51
aryanadepu373 <aryuuscode@gmail.com>
2026-09-05 21:29:35 +0530

    Time: 4 ms (24.57%) | Memory: 53.4 MB (98.01%) - LeetSync

---
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.c
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* swapNodes(struct ListNode* head, int k) {
+    struct ListNode* ptr1,*ptr2,*temp;
+    temp=head;
+    int count=0;
+    while(temp!=NULL){
+        count++;
+        temp=temp->next;
+    }
+    int klast=count-k+1;
+    temp=head;
+    count=1;
+    while(temp!=NULL){
+        if(count==k){
+            ptr1=temp;
+        }
+        if(count==klast){
+            ptr2=temp;
+        }
+        count++;
+        temp=temp->next;
+    }
+    int val=ptr1->val;
+    ptr1->val=ptr2->val;
+    ptr2->val=val;
+    return head;
+}
